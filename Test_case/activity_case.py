a6b91b688e9c12481de8d4078e1f916fd0b5d597
ZF <870951561@qq.com>
2021-05-25 19:09:34 +0800

    添加测试套件

--- Test_case/activity_case.py
+++ Test_case/activity_case.py
@@ -10,13 +10,16 @@ from ddt import ddt, file_data
 class Activity_TestRun(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+        print('测试1')
         cls.driver = webdriver.Chrome()
 
     def setUp(self):
+        print('测试2')
         self.activity_case = Activity(self.driver)
 
     @file_data('../Data/activity.yaml')
     def test_new_group(self, **kwargs):
+        print('测试3')
         """新建拼团活动"""
         # 活动管理
         activitys = kwargs['activitys']
@@ -32,19 +35,19 @@ class Activity_TestRun(unittest.TestCase):
         new_texttwo = self.activity_case.new_texttwo
         self.assertEqual(first=new_textone, second=new_texttwo, msg='新建拼团失败！')
 
-    @file_data('../Data/activity.yaml')
-    def test_delete_group(self, **kwargs):
-        """删除拼团"""
-        # 活动管理
-        activitys = kwargs['activitys']
-        # 拼团活动
-        grouds = activitys['grouds']
-        # 新建拼团活动
-        grouds_goods = grouds['grouds_goods']
-        self.activity_case.delete_group(groud_el=grouds_goods)
-        delete_text_one = self.activity_case.delete_text_one
-        delete_text_two = self.activity_case.delete_text_two
-        self.assertEqual(first=delete_text_one, second=delete_text_two, msg='删除拼团失败！')
+    # @file_data('../Data/activity.yaml')
+    # def test_delete_group(self, **kwargs):
+    #     """删除拼团"""
+    #     # 活动管理
+    #     activitys = kwargs['activitys']
+    #     # 拼团活动
+    #     grouds = activitys['grouds']
+    #     # 新建拼团活动
+    #     grouds_goods = grouds['grouds_goods']
+    #     self.activity_case.delete_group(groud_el=grouds_goods)
+    #     delete_text_one = self.activity_case.delete_text_one
+    #     delete_text_two = self.activity_case.delete_text_two
+    #     self.assertEqual(first=delete_text_one, second=delete_text_two, msg='删除拼团失败！')
 
 
 if __name__ == '__main__':
--- Test_run/test_run.py
+++ Test_run/test_run.py
@@ -1,29 +1,12 @@
 import unittest
-from Test_case.Login_case import *
-from Test_case.activity_case import *
+import os
 
-
-class Test_Server(unittest.TestCase):
-    # 实例化测试套件
-    @classmethod
-    def setUpClass(cls):
-        print(1111111)
-        cls.suite = unittest.TestSuite()
-
-    def setUp(self):
-        print(22222222222)
-        self.suite.addTest(Login('test_open'))
-
-    def tearDown(self):
-        print(33333333333)
-        self.suite.addTest(Login('test_over'))
-
-    def test_groudcase(self):
-        print(4444444444)
-        self.suite.addTest(Activity_TestRun('test_newgroud'))
+case_path = os.path.abspath(os.path.join(os.getcwd(), "../Test_case"))
+# test_dir = '/.'
+discover = unittest.defaultTestLoader.discover(case_path, pattern='*_case.py', top_level_dir=None)
 
 
 if __name__ == '__main__':
     # unittest.main()
     Test_runner = unittest.TextTestRunner()
-    Test_runner.run(unittest.TestCase())
+    Test_runner.run(discover)
